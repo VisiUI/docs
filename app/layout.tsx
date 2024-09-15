41e1b9a1b2cd43707b78456158c78e08309ce860
mineshopdev <mineshop47@gmail.com>
2024-09-15 19:45:39 +0200

    Added site name and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,13 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Docs",
-  description: "",
+  description: "There you can find all the VisiUI's components with Preview, Code, Usage Methods and Usage",
   icons: "https://raw.githubusercontent.com/VisiUI/VisiUI/main/public/visiui8.png",
   openGraph: {
     title: "VisiUI Docs",
-    description: "",
+    description: "There you can find all the VisiUI's components with Preview, Code, Usage Methods and Usage",
     url: "https://docs-visi-ui.vercel.app/docs",
+    siteName: "VisiUI Docs",
     images: [
       {
         url: "https://docs-visi-ui.vercel.app/api/og",
